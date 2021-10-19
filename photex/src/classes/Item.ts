1b55992caf0c81f1b3e983886aedfa960639a54c
AstroCoding <37601898+AstroCoding@users.noreply.github.com>
2021-10-18 21:08:52 -0400

    Uploaded basic class files and begun work

---
+++ photex/src/classes/Item.ts
@@ -0,0 +1,12 @@
+interface Item {
+    rotation: number;
+    parent: Item | null;
+    children: Item[];
+    depth: number;
+    id: string;
+    addChild(child: Item): void;
+    render(): string;
+    renderChildren(): string;
+}
+
+export default Item;
